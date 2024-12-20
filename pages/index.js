d59dd69e6e2fd56802aff84903d05ca4bf455519
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-12-20 11:00:46 -0800

    Add FAQs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Box, Heading } from "theme-ui";
+import { Box, Card, Grid, Heading } from "theme-ui";
 
 export default function Home() {
     return (
@@ -46,26 +46,31 @@ export default function Home() {
                     position: "absolute",
                     top: "0px",
                     right: "0px",
-                    transform: "translateY(-20%)"
+                    width: "25vw",
+                    transform: "translateY(-20%)",
+                    zIndex: 7
                 }} />
-                <img src="/2.png" style={{
+                <img src="/2.png" style={{ // post it note
                     position: "absolute",
                     top: "0px",
-                    left: "15%",
+                    left: "max(12vw, 20px)",
                     transform: "translateY(-20%)",
-                    width: "400px"
+                    width: "max(40vw, 300px)",
+                    zIndex: 9
                 }} />
                 <img src="/paperball1.svg" style={{
                     position: "absolute",
-                    top: "200px",
-                    left: "5%",
-                    width: "300px"
+                    top: "10vw",
+                    left: "-2vw",
+                    width: "20vw",
+                    zIndex: 6
                 }} />
                 <img src="/paperball2.svg" style={{
                     position: "absolute",
-                    top: "250px",
-                    left: "55%",
-                    width: "300px"
+                    top: "10vw",
+                    left: "48vw",
+                    width: "26vw",
+                    zIndex: 6
                 }} />
                 <Box sx={{
                     background: "url('/note.png')",
@@ -74,6 +79,51 @@ export default function Home() {
                     <Heading as="h1">February 15-16, 2024</Heading>
                 </Box>
                 Scrapyard!
+            </Box>
+            <Box sx={{
+                width: "100%",
+                background: "url('/navy_paper_bg.svg')",
+                backgroundSize: "cover",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                gap: "20px",
+                py: 6,
+                px: 4,
+                position: "relative",
+            }}>
+                <Heading as="h1" sx={{
+                    color: "white"
+                }}>Scrapyard FAQs</Heading>
+                <Grid columns={[1, 2, 3]} gap={4}>
+                    <Card sx={{
+
+                    }}>
+                        <Heading as="h2">What is Scrapyard?</Heading>
+                        <p>Scrapyard is a hackathon for high school students, organized by the students of the BRT High School.</p>
+                    </Card>
+                    <Card>
+                        <Heading as="h2">When is Scrapyard?</Heading>
+                        <p>Scrapyard will be held on February 15-16, 2024.</p>
+                    </Card>
+                    <Card>
+                        <Heading as="h2">Where is Scrapyard?</Heading>
+                        <p>Scrapyard will be held at the BRT High School.</p>
+                    </Card>
+                    <Card>
+                        <Heading as="h2">How can I participate?</Heading>
+                        <p>Participation details will be announced soon.</p>
+                    </Card>
+                    <Card>
+                        <Heading as="h2">What are the prizes?</Heading>
+                        <p>Prizes will be announced soon.</p>
+                    </Card>
+                    <Card>
+                        <Heading as="h2">How can I sponsor Scrapyard?</Heading>
+                        <p>Contact us at <a href="mailto:scrapyard@hackclub.com">scrapyard@hackclub.com</a></p>
+                    </Card>
+                </Grid>
             </Box>
         </>
     )
