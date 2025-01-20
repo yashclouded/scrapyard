6a51f2dc55633a141c19e6d503a236a24b867ba6
polypixeldev <me@sfernandez.dev>
2025-01-20 14:09:32 -0500

    fix other signup link

--- pages/vanity.js
+++ pages/vanity.js
@@ -760,8 +760,7 @@ export default function ExampleCity() {
           })}
         </Grid>
         <Link
-          // TODO: Change [EVENTID] to your event's ID (see https://airtable.com/appigKiF7GbVISAlg/shrK3OiCJs4BRBIRG)
-          href="https://forms.hackclub.com/scrapyard-signup?event_id=[EVENTID]"
+          href="https://forms.hackclub.com/scrapyard-signup?event=vanity"
           target="_blank"
         >
           <Box
