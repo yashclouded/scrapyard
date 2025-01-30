be3c7669ebcee7189265b1b59953ef0f52cdd31d
Nur Ahmed <yeasiresha@gmail.com>
2025-01-31 02:33:44 +0600

    scrapyard dhaka

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,23 @@ export default class extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* AOS CSS (Animate On Scroll) - Add CDN Link */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"
+          />
+        </Head>
         <body>
           <InitializeColorMode />
           <Main />
           <NextScript />
+
+          {/* AOS JS (Animate On Scroll) - Add CDN Script */}
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"
+            defer
+          ></script>
         </body>
       </Html>
     )
