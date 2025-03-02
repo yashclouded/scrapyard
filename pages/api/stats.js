727c9f2af4f539bf70812d756f532676744be952
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-03-02 12:15:48 -0600

    changes

---
+++ pages/api/stats.js
@@ -0,0 +1,25 @@
+import AirtablePlus from 'airtable-plus'
+
+export default async function handler (req, res) {
+    if (!process.env.AIRTABLE_API_KEY) {
+        return res.status(500).json({
+            error: "Misconfigured server API key. Report this in #scrapyard"
+        })
+    }
+
+    const attendeesTable = new AirtablePlus({
+        baseID: "appmCFCuFmxkvO1zc",
+        apiKey: process.env.AIRTABLE_API_KEY,
+        tableName: 'User',
+    });
+
+    const total = await attendeesTable.read({
+        filterByFormula: `NOT({Loops - scrapyardFlagshipProjectSubmittedAt} = '')`
+    }).length;
+
+    const count = await attendeesTable.read({
+        filterByFormula: `NOT({Loops - scrapyardProjectYswsEligibleAt} = '')`
+    }).length;
+    
+    return res.status(200).json({ total, count });
+}
